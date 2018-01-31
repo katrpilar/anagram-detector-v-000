3d031e038eecfa31f47a2f6c2bc8441ffc7cfa16
Katrina Brinson <kbrinso@gmail.com>
2018-01-31 21:31:49 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,8 +12,9 @@ class Anagram
     maybe_anagram_array.map!{|i| i.split("")
       #if the item contains all the same letters that @word does
       #then push the item to an array of matches
-      if i.all?{|item| match_word_array.include?(item)}
-        matches << item
+      if i.all?{|item| match_word_array.include?(item)} == true 
+        matches << i
+      end
     }
     return matches
     #if array = [] after the loop then return false 
