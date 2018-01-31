fcad99258503a98102ab2f1821bd27f8d445335d
Katrina Brinson <kbrinso@gmail.com>
2018-01-31 21:35:33 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,12 +9,13 @@ class Anagram
   def match(maybe_anagram_array)
     matches = []
     match_word_array = word.split("")
-    maybe_anagram_array.map!{|i| i.split("")}
-      #if the item contains all the same letters that @word does
-      #then push the item to an array of matches
+    maybe_anagram_array.map!{|i| i.split("")
       if maybe_anagram_array.all?{|item| match_word_array.include?(item)}
         matches << i
       end
+    }
+      #if the item contains all the same letters that @word does
+      #then push the item to an array of matches
     
     return matches
     #if array = [] after the loop then return false 
